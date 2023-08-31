0243b9dc9f30166ba07b79142e68e9c8ebfd4304
wangjunqiang <wangjunqiang@iscas.ac.cn>
2023-08-31 14:53:42 +0800

    add cvitek support

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -320,6 +320,7 @@ struct mmc_host {
 	unsigned int		f_min;
 	unsigned int		f_max;
 	unsigned int		f_init;
+	unsigned int		f_src;
 	u32			ocr_avail;
 	u32			ocr_avail_sdio;	/* SDIO-specific OCR */
 	u32			ocr_avail_sd;	/* SD-specific OCR */
@@ -518,6 +519,7 @@ struct mmc_host {
 	struct blk_crypto_profile crypto_profile;
 #endif
 
+	bool			ever_unplugged;
 	/* Host Software Queue support */
 	bool			hsq_enabled;
 
